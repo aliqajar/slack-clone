6560f8d0c82deb149df17185dbe22a97a7c3327a
aliqajar <ali.qajar@gmail.com>
2021-07-04 00:48:17 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's build slack</h1>
+    <div className="app">
+      <Router>
+        <>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/" exact>
+              <Header/>
+            </Route>
+          </Switch>
+        </>
+      </Router>      
     </div>
   );
 }
